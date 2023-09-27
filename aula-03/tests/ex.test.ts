720a6dc36c9752d3b26044c3347451b1c407510c
andressafirmino <andressafirmino78@gmail.com>
2023-09-27 20:38:24 -0300

    aula-03

---
+++ aula-03/tests/ex.test.ts
@@ -0,0 +1,14 @@
+describe("aula 03", () => {
+    it("função soma", () => {
+        expect(2 + 2).toBe(4);
+    })
+    it("função subtração", () => {
+        expect(2 - 2).toBe(0);
+    })
+    it("função multiplicação", () => {
+        expect(2 * 2).toBe(4);
+    })
+    it("função divisão", () => {
+        expect(2 / 2).toBe(1);
+    })
+})
